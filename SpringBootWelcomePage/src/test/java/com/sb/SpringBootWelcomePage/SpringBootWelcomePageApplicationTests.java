a6dfacfd00b4314ee44fcca714dbeac956bf538d
Gaurav Kumar Singh <singhkumargg999@gmail.com>
2021-06-16 14:28:38 +0530

    SpringBoot - Simple Welcome Page

---
+++ SpringBootWelcomePage/src/test/java/com/sb/SpringBootWelcomePage/SpringBootWelcomePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.SpringBootWelcomePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWelcomePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
